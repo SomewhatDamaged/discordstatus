25cd0951e112cdb4f824835073faf3f18a34e15e
SomewhatDamaged <not.2.me.damaged@gmail.com>
2020-09-15 16:33:42 +1000

    Remove comment

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DiscordStatus",  # Replace with your own username
+    name="DiscordStatus",
     version="0.0.1",
     author="Byron Mulvogue",
     author_email="not.2.me.damaged@gmail.com",
